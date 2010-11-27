9d67599922f7dd4618cb7cc13b7b9ed77784e22a
boonebgorges <boonebgorges@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-27 18:14:41 +0000

    Tagging svn version 0.7
    git-svn-id: https://plugins.svn.wordpress.org/invite-anyone/tags/0.7@316126 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- invite-anyone.php
+++ invite-anyone.php
@@ -4,12 +4,12 @@
 Plugin Name: Invite Anyone
 Plugin URI: http://teleogistic.net/code/buddypress/invite-anyone/
 Description: Allows group admins to invite any BuddyPress member to a group, whether or not they are friends
-Version: 0.6.7
+Version: 0.7
 Author: Boone Gorges
 Author URI: http://teleogistic.net
 */
 
-define( 'BP_INVITE_ANYONE_VER', '0.6' );
+define( 'BP_INVITE_ANYONE_VER', '0.7' );
 
 if ( !defined( 'BP_INVITE_ANYONE_SLUG' ) )
 	define( 'BP_INVITE_ANYONE_SLUG', 'invite-anyone' );
@@ -67,7 +67,9 @@ function invite_anyone_activation() {
 
 	if ( !$iaoptions['can_send_group_invites_email'] )
 		$iaoptions['can_send_group_invites_email'] = 'yes';
-
+	
+	if ( !$iaoptions['bypass_registration_lock'] )
+		$iaoptions['bypass_registration_lock'] = 'yes';
 
 	update_option( 'invite_anyone', $iaoptions );
 }
